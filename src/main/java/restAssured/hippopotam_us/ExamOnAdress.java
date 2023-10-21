d0d8b4eff731aa328f49adb2e39cd6c4989b4dc8
Haldun35 <tsmnc054@gmail.com>
2023-10-21 16:20:48 +0200

    21.10.2023 ExamOnHipopotam_usRestAssured

--- src/main/java/restAssured/hippopotam_us/ExamOnAdress.java
+++ src/main/java/restAssured/hippopotam_us/ExamOnAdress.java
@@ -7,6 +7,7 @@ import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.get;
@@ -67,7 +68,7 @@ public class ExamOnAdress {
         int lNum = 35399;
         String url = "https://api.zippopotam.us/TR/{pc}";
 
-        for (int i = num; i < lNum ; i+= 5) {
+        for (int i = num; i < lNum; i += 5) {
 
             given()
                     .pathParam("pc", i)
@@ -79,23 +80,81 @@ public class ExamOnAdress {
 
 
     }
+
     @Test
-    public void ExamOnAdressForAssert(){
+    public void ExamOnAdressForAssert() {
         given()
                 .get("https://api.zippopotam.us/TR/35380")
                 .then()
                 .contentType(ContentType.JSON)
                 .log().body()
                 .statusCode(200)
-                .body("$",hasKey("post code"))
+                .body("$", hasKey("post code"))
                 .body("country", equalTo("Turkey"))
                 //.body("country abbreviation",equalTo("TR"))
-                .body("places[0].state",equalTo("İzmir"))
+                .body("places[0].state", equalTo("İzmir"))
                 //.body("latitude",equalTo("40.35"))
-                .body("places.state",hasItems("İzmir"))
+                .body("places.state", hasItems("İzmir"))
                 //.body("places.state abbreviation",hasItems("35"))
                 .body(containsString("post code"))
                 .body(containsString("37.55"))
+
+
+        ;
+    }
+
+
+    /*public void forLoop(){
+
+        int num = 35380;
+        int lNum = 35399;
+
+        for (int i = num; i < lNum ; i+=5) {
+            autoCheckAssert (i);
+        }
+    }*/
+
+    @Test
+    public void autoCheckAssert() {
+
+
+
+        int num = 35380;
+        int lNum = 35383;
+
+        String url = "https://api.zippopotam.us/TR/{pc}";
+
+        for (int i = num; i < lNum; i ++) {
+            Response response = given().pathParam("pc",i).when().get(url);
+
+            if (response.body() != null){
+            given()
+                    .pathParam("pc", i)
+                    .when()
+                    .get(url)
+                    .prettyPrint();
+        }
+        }
+
+
+
+        /*do {
+            given()
+                    .pathParam("pc", number)
+                    .when()
+                    .get(url)
+                    .prettyPrint();
+
+        }while (given().get(url).then().body(containsString("places")) != "" )*/
+
+
+
+
+
+
+
+
+
 
 
 
@@ -107,5 +166,49 @@ public class ExamOnAdress {
 
         ;
     }
+    @Test
+    public void autoCheckAssertdeneme() {
 
+
+
+        int num = 35380;
+        int lNum = 35383;
+        int count = 0;
+
+        String url = "https://api.zippopotam.us/TR/{pc}";
+
+        for (int i = num; i < lNum; i ++) {
+            Response response = given().pathParam("pc",i).when().get(url)
+                    .then().extract().response();
+            String str = response.body().prettyPrint().toString();
+
+
+            if (str != ""){
+                given()
+                        .pathParam("pc", i)
+                        .when()
+                        .get(url)
+                        .prettyPrint();
+                count++;
+            }
+        }
+        System.out.println(count);
+
+}
+    @Test
+    public void _autoCheckAssert() {
+
+
+
+
+
+        String url = "https://api.zippopotam.us/TR/35381";
+        Response response = get(url)
+                .then().extract().response();
+        response.prettyPrint();
+
+
+
+
+        }
 }
